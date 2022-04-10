081237d9eb9b36ae7f9f7c157ad1fcb8c56b8538
Ready0rN0t21 <shawn.ray.aganan11@gmail.com>
2022-04-10 14:55:37 +0800

    initialize project

--- src/main/java/com/alliance/claimsvalidationapp/repository/UserRepository.java
+++ src/main/java/com/alliance/claimsvalidationapp/repository/UserRepository.java
@@ -12,4 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
+    User findByEmailAndPassword(String email, String pass);
+
+
 }
--- src/main/java/com/alliance/claimsvalidationapp/service/UserService.java
+++ src/main/java/com/alliance/claimsvalidationapp/service/UserService.java
@@ -5,17 +5,72 @@ import com.alliance.claimsvalidationapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
     public User registerUserService(User user){
-        User tempUser = userRepository.findByEmail(user.getEmail()).get();
 
-        if(userRepository.existsById(tempUser.getId())){
+        if(userRepository.findByEmail(user.getEmail()).isPresent()){
             throw new IllegalStateException("Email already exist");
         }
+        user.setUserStatus("active");
         return userRepository.save(user);
     }
+
+    public User loginUserService(String email, String pass){
+        User user = userRepository.findByEmailAndPassword(email, pass);
+        if(userRepository.existsById(user.getId())) {
+            if(!user.getUserStatus().equals("deleted"))
+                return user;
+        }
+        throw new IllegalStateException("User not found!");
+    }
+
+    public User updateUserService(Long id, User user){
+        User oldUser = null;
+        if(userRepository.findById(id).isPresent()){
+            oldUser = userRepository.findById(id).get();
+
+            if((user.getFirstName() == null || user.getLastName() == null) && user.getPassword() == null){
+                throw new IllegalStateException("Please fill all the fields");
+            }
+
+            if(user.getPassword() != null && (user.getFirstName() == null && user.getLastName() == null)){
+                oldUser.setPassword(user.getPassword());
+            }
+            if (user.getFirstName() != null && user.getLastName() != null && (user.getPassword() == null)){
+                oldUser.setFirstName(user.getFirstName());
+                oldUser.setLastName(user.getLastName());
+            }
+
+            if (user.getFirstName() != null && user.getLastName() != null && user.getPassword() != null) {
+                oldUser.setFirstName(user.getFirstName());
+                oldUser.setLastName(user.getLastName());
+                oldUser.setPassword(user.getPassword());
+            }
+        } else {
+            throw new IllegalStateException("User not found");
+        }
+        return userRepository.save(oldUser);
+    }
+
+    public User deleteUserService(Long id){
+        User oldUser = null;
+        if(userRepository.findById(id).isPresent()){
+            oldUser = userRepository.findById(id).get();
+            oldUser.setUserStatus("deleted");
+        } else {
+            throw new IllegalStateException("User not found");
+        }
+        return userRepository.save(oldUser);
+    }
+
+//    public List<User> getAllUserService(){
+//
+//    }
+
 }
